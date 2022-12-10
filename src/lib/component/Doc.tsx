db496f33d624d49c1ce4d9b67062807c9ec70544
Sim Ho <simhozebs@gmail.com>
2022-12-10 16:30:34 -0500

    document save btn

--- src/lib/component/Doc.tsx
+++ src/lib/component/Doc.tsx
@@ -2,14 +2,20 @@ import React, { useRef, useState } from "react";
 import { createEditor, Descendant } from "slate";
 import { Slate, Editable, withReact, RenderElementProps } from "slate-react";
 import { VFile } from "vfile/lib";
+import { writeTextFile, BaseDirectory } from "@tauri-apps/api/fs";
+import { serialize } from "remark-slate";
 
 const Doc = ({ file }: { file: VFile }) => {
-  const [title, setTitle] = useState(file.dirname);
+  const [title, setTitle] = useState(file.basename);
   const initialValue = useRef(file.result as Descendant[]);
 
   const [editor] = useState(() => withReact(createEditor()));
 
-  const saveDoc = () => {};
+  const saveDoc = () => {
+    const content = editor.children.map((child) => serialize(child)).join("");
+    writeTextFile("Basalt/test.md", content, { dir: BaseDirectory.Data });
+  };
+
   const renderElement = (props: RenderElementProps) => {
     switch (props.element.type) {
       case "heading_one":
@@ -26,11 +32,7 @@ const Doc = ({ file }: { file: VFile }) => {
   return (
     <div className="flex h-max w-full flex-col bg-inherit p-3">
       <div>
-        <input
-          className="bg-inherit text-4xl"
-          value={title}
-          onChange={(e) => setTitle(e.currentTarget.value)}
-        />
+        <input className="bg-inherit text-4xl" value={title} />
         <button onClick={saveDoc}>save</button>
       </div>
 
