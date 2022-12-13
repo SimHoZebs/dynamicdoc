dcee28b32273be42baf30baf5bfb5d4673c90fe3
Sim Ho <simhozebs@gmail.com>
2022-12-12 21:21:37 -0500

    return to web

--- src/server/routers/page.ts
+++ src/server/routers/page.ts
@@ -1,6 +1,6 @@
 import { procedure, router } from "../trpc";
-import { z } from 'zod';
-import db from '../../lib/util/db';
+import { z } from "zod";
+import db from "../../lib/util/db";
 
 const pageRouter = router({
   get: procedure
@@ -8,45 +8,44 @@ const pageRouter = router({
     .query(async ({ input }) => {
       const page = await db.page.findFirst({
         where: {
-          ...input
+          ...input,
         },
         include: {
-          blockArray: true
-        }
+          blockArray: true,
+        },
       });
       if (!page) return page;
 
-      const blockOrder = page.blockOrder.split(",").map((id) => (id));
-      const sortedBlockArray = blockOrder.map((id) => {
-        const block = page.blockArray.find(block => block.id === id);
+      const sortedBlockArray = page.blockOrder.map((id) => {
+        const block = page.blockArray.find((block) => block.id === id);
         return block
           ? block
           : {
-            id: id,
-            type: "",
-            pageId: input.id,
-            content: "This line was found, but could not find the content."
-          };
+              id: id,
+              type: "",
+              pageId: input.id,
+              content: "This line was found, but could not find the content.",
+            };
       });
 
       page.blockArray = sortedBlockArray;
       return page;
     }),
 
-  getAll: procedure
-    .input(z.void())
-    .query(() => {
-      return db.page.findMany();
-    }),
+  getAll: procedure.input(z.void()).query(() => {
+    return db.page.findMany();
+  }),
 
   create: procedure
-    .input(z.object({
-      title: z.string(),
-      blockOrder: z.string()
-    }))
+    .input(
+      z.object({
+        title: z.string(),
+        blockOrder: z.string(),
+      })
+    )
     .mutation(({ input }) => {
       return db.page.create({
-        data: input
+        data: input,
       });
     }),
 
@@ -54,18 +53,18 @@ const pageRouter = router({
     .input(
       z.object({
         pageId: z.string(),
-        blockOrder: z.string()
+        blockOrder: z.string(),
       })
     )
     .mutation(({ input }) => {
       return db.page.update({
         where: {
-          id: input.pageId
+          id: input.pageId,
         },
         data: {
-          blockOrder: input.blockOrder
-        }
+          blockOrder: input.blockOrder,
+        },
       });
-    })
+    }),
 });
 export default pageRouter;
