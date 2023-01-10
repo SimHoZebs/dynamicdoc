2d849a713d19fe686e8c7914ecad97cfb2280ae8
Sim Ho <simhozebs@gmail.com>
2023-01-09 21:59:14 -0500

    remove slate

--- src/lib/component/Heading.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { RenderElementProps } from "slate-react";
-
-const Heading = (props: RenderElementProps) => {
-  return <h1 {...props.attributes}>{props.children}</h1>;
-};
-
-export default Heading;
